f860cef80714461bb0a1e3b75e1d09e891b2aa2c
Jean-Christophe Proulx <j.christophe@devjc.net>
2019-07-03 15:33:58 -0400

    Update lftp-mirror.sh

--- lftp-mirror.sh
+++ lftp-mirror.sh
@@ -5,16 +5,13 @@ echo -e "Variables set:\\n\
 
 PUID=${PUID}\\n\
 PGID=${PGID}\\n\
-
 HOST=${HOST}\\n\
 USERNAME=${USERNAME}\\n\
 PASSWORD=${PASSWORD}\\n\
-
 REMOTE_DIR=${REMOTE_DIR}\\n\
 FINISHED_DIR=${FINISHED_DIR}\\n\
 LFTP_PARTS=${LFTP_PARTS}\\n\
 LFTP_FILES=${LFTP_FILES}\\n"
-
 BASE_NAME="$(basename "$0")"
 LOCK_FILE="/tmp/$BASE_NAME.lock"
 
@@ -33,53 +30,52 @@ mkdir -p /config/.download
 # create finished downloads directory
 mkdir -p /config/download
 
-# Cycle every minute
+# cycle every minute
 while true
 do
-
-trap "rm -f $LOCK_FILE" SIGINT SIGTERM
-if [[ -e "$LOCK_FILE" ]]
-then
-    echo "[$(date '+%H:%M:%S')] $BASE_NAME is running already."
-else
-    touch "$LOCK_FILE"
-	echo "[$(date '+%H:%M:%S')] Created lock file."
-	
-	echo "[$(date '+%H:%M:%S')] Initiating connection to $host using sftp"	
-	
-    lftp -u $USERNAME,$PASSWORD $HOST << EOF
-    set sftp:auto-confirm yes
-    set mirror:use-pget-n $LFTP_PARTS
-	set net:connection-limit 50
-	set xfer:log 1
-	set xfer:eta-period 5 
-	set xfer:use-temp-file yes
-	set pget:save-status never
-	mirror -v -P$LFTP_FILES --log="/config/$BASE_NAME.log" $REMOTE_DIR /config/.download
-    quit
-EOF
- 
-for file in /config/.download/*; do	
+	trap "rm -f $LOCK_FILE" SIGINT SIGTERM
+	if [[ -e "$LOCK_FILE" ]]
+	then
+		echo "[$(date '+%H:%M:%S')] $BASE_NAME is running already."
+	else
+		touch "$LOCK_FILE"
+		echo "[$(date '+%H:%M:%S')] Created lock file."		
+		echo "[$(date '+%H:%M:%S')] Initiating connection to $host using sftp"	
 		
-	if [ "$REMOTE_DIR/*" = "${REMOTE_DIR}/${file##*/}" ]; then
-		echo "[$(date '+%H:%M:%S')] No files were synchronized."
-		rm -f "$LOCK_FILE"
-		trap - SIGINT SIGTERM	
-	else	
-		echo "[$(date '+%H:%M:%S')] Transfers are completed... !"
 		lftp -u $USERNAME,$PASSWORD $HOST << EOF
-		set sftp:auto-confirm yes
-		command  rm "${remote_dir}/${file##*/}" 
-    quit	
-EOF
-fi
-	echo "[$(date '+%H:%M:%S')] Moving files off Sync to Download folder."
-	chmod -R 777 /config/.download/*
-	mv "${/config/.download}/${file##*/}" "$FINISHED_DIR"		
-    rm -f "$LOCK_FILE"
-    trap - SIGINT SIGTERM		
-fi
-    # Repeat process after one minute
-    echo "[$(date '+%H:%M:%S')] Sleeping for 1 minute"
-    sleep 1m
+			set sftp:auto-confirm yes
+			set mirror:use-pget-n $LFTP_PARTS
+			set net:connection-limit 50
+			set xfer:log 1
+			set xfer:eta-period 5 
+			set xfer:use-temp-file yes
+			set pget:save-status never
+			mirror -v -P$LFTP_FILES --log="/config/$BASE_NAME.log" $REMOTE_DIR /config/.download    
+		EOF
+	 
+		for file in /config/.download/*; do				
+			if [ "$REMOTE_DIR/*" = "${REMOTE_DIR}/${file##*/}" ]; then
+				echo "[$(date '+%H:%M:%S')] No files were synchronized."
+				rm -f "$LOCK_FILE"
+				trap - SIGINT SIGTERM	
+			else	
+				echo "[$(date '+%H:%M:%S')] Transfers are completed... !"
+				lftp -u $USERNAME,$PASSWORD $HOST << EOF
+					set sftp:auto-confirm yes
+					command  rm "${remote_dir}/${file##*/}"     	
+				EOF
+				
+				echo "[$(date '+%H:%M:%S')] Moving files off Sync to Download folder."
+				chmod -R 777 /config/.download/*
+				mv "${/config/.download}/${file##*/}" "$FINISHED_DIR"	
+			fi			
+			
+			rm -f "$LOCK_FILE"
+			trap - SIGINT SIGTERM
+		done
+		
+	fi
+		# Repeat process after one minute
+		echo "[$(date '+%H:%M:%S')] Sleeping for 1 minute"
+		sleep 1m
 done
